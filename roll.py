fab9579fe43e6c0597646aa6ff1223e069020e3b
Imir <imir@Zipcoders-MacBook-Pro-4.local>
2023-11-14 15:29:05 -0500

    imported random, making functions

--- roll.py
+++ roll.py
@@ -1,3 +1,5 @@
+import random
+
 # roll a "die" some number of times.
 # roll - run it once and go into a loop
 # roll 1 - produce a number 1-6 random and print it
@@ -5,3 +7,13 @@
 # roll 3 - produce three numbers and print them.
 #
 
+random_number1 = random.randint(1, 7)
+random_number2 = random.randint(1, 7)
+random_number3 = random.randint(1, 7)
+random_number4 = random.randint(1, 7)
+
+def dice(roll_dice):
+    if roll_dice == 1:
+        print(f"Here is your dice roll: {random_number1}")
+    elif roll_dice == 2:
+        print(f"Here's")
